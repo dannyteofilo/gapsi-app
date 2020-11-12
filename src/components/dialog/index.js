0a177e2a7e0478e75891a0a75d644c26690511c2
Teofilo <danyel.285309@gmail.com>
2020-11-11 22:30:53 -0600

    fix: adding react scripts and enviroment

---
+++ src/components/dialog/index.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Slide from '@material-ui/core/Slide';
+
+function Transition(props) {
+    return <Slide direction="up" {...props} />;
+}
+
+class AlertDialogSlide extends React.Component {
+    state = {
+        open: false,
+        title: '',
+        message: ''
+    };
+
+    handleClickOpen = (title, message) => {
+        this.setState({ open: true, title, message });
+    };
+
+    handleClose = () => {
+        this.setState({ open: false });
+    };
+
+
+    render() {
+        const { title, message } = this.state
+        return (
+            <div>
+                <Dialog
+                    open={this.state.open}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    onClose={this.handleClose}
+                    aria-labelledby="alert-dialog-slide-title"
+                    aria-describedby="alert-dialog-slide-description"
+                >
+                    <DialogTitle id="alert-dialog-slide-title">
+                        {title}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-slide-description">
+                            {message}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.handleClose} color="primary">
+                            OK
+            </Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
+        );
+    }
+}
+
+export default AlertDialogSlide;
