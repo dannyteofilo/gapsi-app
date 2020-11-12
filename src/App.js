3f67afc842c61acec00d0fdb97be715bf47d94da
Teofilo <danyel.285309@gmail.com>
2020-11-11 22:29:42 -0600

    feat: adding router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import './App.css';
-import { Provider } from "react-redux";
-import { getStore } from "./shared/redux/store";
+import { Provider } from 'react-redux';
+import { getStore } from './shared/redux/store';
+import { Route, HashRouter, Switch } from 'react-router-dom';
+import Login from 'login'
+import GuardedRoute from 'hocs';
+import Layout from 'layout'
+
+
 
 
 const theme = createMuiTheme({
-    palette:{
-      primary :  {main: '#ff5722'},
-      secondary : {main: '#00b0ff'}      
-    }
+  palette: {
+    primary: { main: '#ff5722' },
+    secondary: { main: '#00b0ff' }
+  }
 });
 
-class  App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <Provider store={getStore()}>
         <MuiThemeProvider theme={theme}>
-          <div>
-            Gapsi
-          </div>
+          <HashRouter>
+            <Switch>
+              <Route path='/auth/login' component={Login} />
+              <GuardedRoute path='/' component={Layout} />
+            </Switch>
+          </HashRouter>
         </MuiThemeProvider>
       </Provider>
     );
--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import configureStore from './shared/redux/store';
 
+
+async function init() {
+  console.log('Waiting for store');
+
+  await configureStore();
+  console.log('Store was fully loaded');
+
+  ReactDOM.render(<App />, document.getElementById('root'));
+  console.log('rendering');
+
+  reportWebVitals();
+}
+init();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
