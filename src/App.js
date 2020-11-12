90d17611ffe8252a4e26bcdf661f73d08f40efde
Teofilo <danyel.285309@gmail.com>
2020-11-11 21:08:59 -0600

    feat: redux store is configured

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
+import React, { Component } from "react";
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core';
 import './App.css';
+import { Provider } from "react-redux";
+import { getStore } from "./shared/redux/store";
 
-function App() {
-  return (
-    <div>Gapsi</div>
-  );
+
+const theme = createMuiTheme({
+    palette:{
+      primary :  {main: '#ff5722'},
+      secondary : {main: '#00b0ff'}      
+    }
+});
+
+class  App extends Component{
+  render(){
+    return(
+      <Provider store={getStore()}>
+        <MuiThemeProvider theme={theme}>
+          <div>
+            Gapsi
+          </div>
+        </MuiThemeProvider>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ src/shared/redux/store.js
@@ -0,0 +1,44 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import createSagaMiddleware from "redux-saga";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+
+import rootReducer from "./rootReducer";
+import rootSaga from "./rootSaga";
+
+const persistConfig = {
+    key: "root",
+    storage
+};
+
+let store = null;
+
+const configureStore = () => {
+    return new Promise((resolve, reject) => {
+        try {
+            const persistedReducer = persistReducer(persistConfig, rootReducer);
+            const sagaMiddleware = createSagaMiddleware();
+
+            store = createStore(
+                persistedReducer,
+                undefined,
+                compose(
+                    applyMiddleware(logger, sagaMiddleware)
+                )
+            );
+
+            persistStore(store, null, () => setTimeout(() => resolve(store)));
+
+            sagaMiddleware.run(rootSaga);
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+export const getStore = () => {
+    return store;
+};
+
+export default configureStore;
