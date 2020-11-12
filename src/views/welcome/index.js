bbc142f2325acd2c6215efa9c54f87a7041c0d1c
Teofilo <danyel.285309@gmail.com>
2020-11-11 22:26:50 -0600

    feat: component views added

---
+++ src/views/welcome/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+
+class Welcome extends Component{
+    render(){
+        return(
+            <div>Welcome to gapsi app</div>
+        );
+    }
+}
+
+
+export default Welcome;
