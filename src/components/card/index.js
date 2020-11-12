dae5969c80ddd9e2b8f7efa5cf5e6c23896ab79e
Teofilo <danyel.285309@gmail.com>
2020-11-12 02:45:29 -0600

    feat: adding card component

---
+++ src/components/card/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: 140,
+    },
+});
+
+const MediaCard = (props) => {
+    const classes = useStyles();
+
+    const { description, image, name, price, sku } = props;
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    className={classes.media}
+                    image={image}
+                    title={name}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {name}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {price}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {sku}
+                </Typography>
+                <Button variant="contained" size="small" color="secondary">
+                    Agregar
+                </Button>
+            </CardActions>
+        </Card>
+    );
+}
+
+export default MediaCard;
