d74aaf3f8761f48f5ecafada7bacc8be8b64ded5
Teofilo <danyel.285309@gmail.com>
2020-11-11 22:24:09 -0600

    feat: componente layout added

---
+++ src/layout/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import Welcome from 'views/welcome'
+
+class Layout extends Component {
+    render() {
+        return (
+            <div>
+                <Switch>
+                    <Route path="/welcome" name="Welcome" component={Welcome} />
+                    <Redirect from="/" to="/home" />
+                </Switch>
+            </div>
+        )
+    }
+}
+
+
+export default Layout;
